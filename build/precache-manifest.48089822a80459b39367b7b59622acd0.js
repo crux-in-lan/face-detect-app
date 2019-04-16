4399168cef684f25459818d350f7eae0e52cb264
crux-in-lan <nicksona.in.lan@gmail.com>
2019-04-16 23:26:35 +0300

    The bounding box issue was fixed

---
+++ build/precache-manifest.48089822a80459b39367b7b59622acd0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/webapps/face-detect-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "8ec783fb59dbd0fdf50c",
+    "url": "/webapps/face-detect-app/static/css/main.54a25e8a.chunk.css"
+  },
+  {
+    "revision": "fc32777b04de4b8cd7c7",
+    "url": "/webapps/face-detect-app/static/js/runtime~main.faf43074.js"
+  },
+  {
+    "revision": "355057a44338eccdd7c6",
+    "url": "/webapps/face-detect-app/static/js/2.37d90546.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/webapps/face-detect-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8ec783fb59dbd0fdf50c",
+    "url": "/webapps/face-detect-app/static/js/main.3ac85bba.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/webapps/face-detect-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/webapps/face-detect-app/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/webapps/face-detect-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/webapps/face-detect-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "feb84efe4a91974a7ad2426258cf3d32",
+    "url": "/webapps/face-detect-app/index.html"
+  }
+];
--- src/home/comPageFrameHome/comHomeFrame/conHomePage/comFaceImage/FaceImage.js
+++ src/home/comPageFrameHome/comHomeFrame/conHomePage/comFaceImage/FaceImage.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-
+import './css/FaceImage.scss';
 class FaceImage extends Component {
 	constructor(props) {
 		super(props);
